7ab3ec3d7b1d543f5557a5d953f45bfa520a515e
ekuznetcov <egor57k@gmail.com>
2021-06-17 20:00:06 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,130 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <v-main>
+      <v-container>
+      <v-row>
+      <v-col cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+      >
+        <v-color-picker v-model="color"></v-color-picker>
+      </v-col>
+      <v-col cols="12"
+        md="6"
+        lg="6"
+      >
+        <v-text-field
+          v-model="itemName"
+          label="Name"
+          :rules="rules"
+          clearable
+          hide-details="auto"
+        ></v-text-field>
+        <v-text-field class="mb-5"
+          v-model="itemType"
+          label="Type"
+          :rules="rules"
+          clearable
+          hide-details="auto"
+        >
+        </v-text-field>
+        <v-btn
+          @click="saveItems"
+        >
+          Save
+        </v-btn>
+      </v-col>
+      </v-row>
+      <data-table
+        :items="items"
+      />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataTable from './components/DataTable.vue';
+import v4 from uuid
 
-export default {
+
+export default {  
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    DataTable
+  },
+
+  data: () => ({
+      types: ['hex', 'hexa', 'rgba', 'hsla', 'hsva'],
+      type: 'hex',
+      hex: '#FF00FF',
+      hexa: '#FF00FFFF',
+      rgba: { r: 255, g: 0, b: 255, a: 1 },
+      hsla: { h: 300, s: 1, l: 0.5, a: 1 },
+      hsva: { h: 300, s: 1, v: 1, a: 1 },
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+      itemName: '',
+      itemType: '',
+      temp: [],
+  }),
+
+  computed: {
+      color: {
+        get () {
+          return this[this.type]
+        },
+        set (value) {
+          this[this.type] = value
+        },
+      },
+      
+      items(){
+        try{
+          let temp = this.temp
+          let items = localStorage.getItem('items')
+          if (items === null || items === undefined)
+            items = []
+          console.log( JSON.parse(items) )
+          return JSON.parse(items)
+        }catch(err){
+          console.error('Не удалось получить достать данные')
+          return []
+        }
+      },
+
+      showColor() {
+        if (typeof this.color === 'string') return this.color
+
+        return JSON.stringify(Object.keys(this.color).reduce((color, key) => {
+          color[key] = Number(this.color[key].toFixed(2))
+          return color
+        }, {}), null, 2)
+      },     
+  },
+
+  methods:{
+    saveItems(){
+        if(this.itemType.length == 0 || this.itemName == 0){
+          this.$emit('empty-input')
+          return
+        }
+        let items = this.items
+        let newItem = {
+          id: v4(),
+          name: this.itemName,
+          type: this.itemType,
+          color: this.color
+        }
+        items.push(newItem)
+        this.temp = items
+        localStorage.setItem('items', JSON.stringify(items) )
+    },
+  },
+};
+</script>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,219 @@
+<template>
+  <v-container fluid>
+    <v-data-iterator
+      :items="items"
+      :items-per-page.sync="itemsPerPage"
+      :page.sync="page"
+      :search="search"
+      :sort-by="sortBy"
+      :sort-desc="sortDesc"
+      hide-default-footer
+    >
+      <template v-slot:header>
+        <v-toolbar
+          light
+          class="mb-1"
+        >
+          <v-text-field
+            v-model="search"
+            clearable
+            flat
+            hide-details
+            color="deep-purple acent-3"
+            prepend-inner-icon="mdi-magnify"
+            label="Search"
+          ></v-text-field>
+          <template v-if="$vuetify.breakpoint.mdAndUp">
+            <v-spacer></v-spacer>
+            <v-select
+              v-model="sortBy"
+              flat 
+              menu-props=auto
+              hide-details
+              :items="keys"
+              color="deep-purple acent-3"
+              label="Sort by"
+            ></v-select>
+            <v-spacer></v-spacer>
+            <v-btn-toggle
+              v-model="sortDesc"
+              mandatory
+            >
+              <v-btn
+                large
+                outlined
+                color="deep-purple accent-3"
+                :value="false"
+              >
+                <v-icon color="deep-purple accent-3">mdi-arrow-up</v-icon>
+              </v-btn>
+              <v-btn
+                large
+                outlined
+                color="deep-purple accent-3"
+                :value="true"
+              >
+                <v-icon color="deep-purple accent-3">mdi-arrow-down</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+          </template>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:default="props">
+        <v-row>
+          <v-col
+            v-for="item, i in props.items"
+            :key="i"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <v-card>
+              <v-card-title class="subheading grey--text">
+                {{ item.name }}
+              </v-card-title>
+
+              <v-divider></v-divider>
+
+              <v-list dense>
+                <v-list-item
+                  v-for="(key, index) in filteredKeys"
+                  :key="index"
+                >
+                  <v-list-item-content :class="{ 'deep-purple--text':sortBy === key }">
+                    {{ key }}:
+                  </v-list-item-content>
+                  <v-list-item-content
+                    class="align-end"
+                    :class="{ 'grey--text': sortBy === key }"
+                  >
+                    {{ item[key] }}
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+
+      <template v-slot:footer>
+        <v-row
+          class="mt-2 mx-1"
+          align="center"
+          justify="center"
+        >
+          <span class="grey--text">Items per page</span>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                dark
+                text
+                color="deep-purple acent3"
+                class="ml-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                {{ itemsPerPage }}
+                <v-icon>mdi-chevron-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(number, index) in itemsPerPageArray"
+                :key="index"
+                @click="updateItemsPerPage(number)"
+              >
+                <v-list-item-title>{{ number }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+
+          <v-spacer></v-spacer>
+
+          <span
+            class="mr-4
+            grey--text"
+          >
+            Page {{ page }} of {{ numberOfPages }}
+          </span>
+          <span>
+          <v-btn
+            fab
+            outlined
+            color="deep-purple accent-3"
+            class="mr-1"
+            @click="formerPage"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            outlined
+            color="deep-purple accent-3"
+            class="ml-1"
+            @click="nextPage"
+          >
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+          </span>
+        </v-row>
+      </template>
+    </v-data-iterator>
+  </v-container>
+</template>
+
+<script>
+
+export default ({
+    props:{
+        items: Array,
+    },
+
+    data:()=>({
+        itemsPerPageArray: [4, 8, 12, 16,],
+        search: '',
+        filter: {},
+        sortDesc: false,
+        page: 1,
+        itemsPerPage: 8,
+        sortBy: 'name',
+        keys: [
+          'name',
+          'type',
+          'color',  
+        ],
+    }),
+
+    computed: {
+      numberOfPages () {
+        return Math.ceil(this.items.length / this.itemsPerPage)
+      },
+      filteredKeys() {
+        return this.keys.filter(key => key !== 'name')
+      },
+    },
+    methods: {
+      nextPage () {
+        if (this.page + 1 <= this.numberOfPages) this.page += 1
+      },
+      formerPage () {
+        if (this.page - 1 >= 1) this.page -= 1
+      },
+      updateItemsPerPage (number) {
+        this.itemsPerPage = number
+      },
+    },
+})
+</script>
+
+<style>
+  .v-list .v-list-item--active { 
+  background-color: rgba(101, 31, 255, 1)!important; 
+  }
+
+  .v-list .v-list-item--active .v-list-item__title {
+    color: white !important;
+  }
+</style>
